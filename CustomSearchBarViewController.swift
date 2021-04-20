52f7d22503893f75b4d223f63573c481df86d81d
Alexander Rofail <75754880+alexrofail@users.noreply.github.com>
2021-04-20 10:34:17 -0400

    Add files via upload

---
+++ CustomSearchBarViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CustomSearchBarViewController.swift
+//  KeydetGoo
+//
+//  Created by brian lipscomb on 2/18/18.
+//  Copyright © 2018 codewithlips. All rights reserved.
+//
+
+import UIKit
+
+class CustomSearchBarViewController: UIViewController {
+    
+    var preferredFont: UIFont!
+    var preferredTextColor: UIColor!
+    
+
+    init(frame: CGRect, font: UIFont, textColor: UIColor) {
+        super.init(frame: frame)
+        
+        self.frame = frame
+        preferredFont = font
+        preferredTextColor = textColor
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
